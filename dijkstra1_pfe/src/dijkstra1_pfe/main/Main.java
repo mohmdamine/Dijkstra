3ff658c35e58920078de577c968ed4329e8bb423
mohmdamine <156541315+mohmdamine@users.noreply.github.com>
2025-04-04 16:16:37 +0200

    Update Main.java

--- dijkstra1_pfe/src/dijkstra1_pfe/main/Main.java
+++ dijkstra1_pfe/src/dijkstra1_pfe/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		
 		Graphe g = new Graphe(9);
 	
-		g.addEdge(0, 1, 4); g.addEdge(0, 7, 8); //0765
+		g.addEdge(0, 1, 4); g.addEdge(0, 7, 8);
 		g.addEdge(1, 2, 8); g.addEdge(1, 7, 11); g.addEdge(2, 1, 8);
 		g.addEdge(2, 8, 2); g.addEdge(2, 5, 4); g.addEdge(2, 3, 7);
 		g.addEdge(3, 2, 7); g.addEdge(3, 5, 14); g.addEdge(3, 4, 9); 
@@ -21,8 +21,8 @@ public class Main {
 		g.addEdge(7, 0, 8); g.addEdge(7, 8, 7); g.addEdge(7, 1, 11); g.addEdge(7, 6, 1);
 		g.addEdge(8, 2, 2); g.addEdge(8, 7, 7); g.addEdge(8, 6, 6);
 		
-		obj.calculer(g.getNoeud(4));
-		Noeud n = g.getNoeud(8);
+		obj.calculer(g.getNoeud(0));
+		Noeud n = g.getNoeud(5);
 		System.out.print("Dist : "+ n.minDistance+" , trajet : ");
 		for(Noeud trajetnoeud:n.trajet) {
 			
