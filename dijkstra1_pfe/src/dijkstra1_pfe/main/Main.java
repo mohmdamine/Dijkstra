a73110a33aa444d43b1839a32abd92fc34230a9d
mohmdamine <156541315+mohmdamine@users.noreply.github.com>
2025-04-04 16:51:35 +0200

    Update Main.java

--- dijkstra1_pfe/src/dijkstra1_pfe/main/Main.java
+++ dijkstra1_pfe/src/dijkstra1_pfe/main/Main.java
@@ -1,5 +1,8 @@
 package dijkstra1_pfe.main;
 
+
+import java.util.Scanner;
+
 import dijkstra1_pfe.dijkstra.Algorithm;
 import dijkstra1_pfe.dijkstra.Graphe;
 import dijkstra1_pfe.dijkstra.Noeud;
@@ -8,10 +11,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Algorithm obj = new Algorithm();
-		
+
 		Graphe g = new Graphe(9);
-	
-		g.addEdge(0, 1, 4); g.addEdge(0, 7, 8);
+		
+		g.addEdge(0, 1, 4); g.addEdge(0, 7, 8); 
 		g.addEdge(1, 2, 8); g.addEdge(1, 7, 11); g.addEdge(2, 1, 8);
 		g.addEdge(2, 8, 2); g.addEdge(2, 5, 4); g.addEdge(2, 3, 7);
 		g.addEdge(3, 2, 7); g.addEdge(3, 5, 14); g.addEdge(3, 4, 9); 
@@ -21,13 +24,21 @@ public class Main {
 		g.addEdge(7, 0, 8); g.addEdge(7, 8, 7); g.addEdge(7, 1, 11); g.addEdge(7, 6, 1);
 		g.addEdge(8, 2, 2); g.addEdge(8, 7, 7); g.addEdge(8, 6, 6);
 		
-		obj.calculer(g.getNoeud(0));
-		Noeud n = g.getNoeud(5);
-		System.out.print("Dist : "+ n.minDistance+" , trajet : ");
+		Scanner src = new Scanner(System.in);
+		System.out.println("entrer le premier noeud");
+		int val = src.nextInt();
+		System.out.println("entrer le deuxieme noeud");
+		int val2 = src.nextInt();
+		
+
+		obj.calculer(g.getNoeud(val));
+		Noeud n = g.getNoeud(val2);
+		System.out.print("Distance : "+ n.minDistance+" , trajet : ");
 		for(Noeud trajetnoeud:n.trajet) {
 			
 			System.out.print(trajetnoeud+" ");
 		}
 		System.out.println(""+n);
 	}
+
 }
