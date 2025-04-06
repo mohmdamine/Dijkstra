a68e8af421019daa99a72cb411cbc7cb0b4dc53a
mohmdamine <156541315+mohmdamine@users.noreply.github.com>
2025-04-06 23:15:04 +0200

    Update Graphe.java

--- dijkstra1_pfe/src/dijkstra1_pfe/dijkstra/Graphe.java
+++ dijkstra1_pfe/src/dijkstra1_pfe/dijkstra/Graphe.java
@@ -1,28 +1,30 @@
 package dijkstra1_pfe.dijkstra;
 
 import java.util.*;
-import java.util.ArrayList;
+
+
 
 public class Graphe {
-	private ArrayList<Noeud> Noeuds;
-	public Graphe(int numberVertices){
-		Noeuds = new ArrayList<Noeud>(numberVertices);
-		for(int i=0;i<numberVertices;i++){
-			Noeuds.add(new Noeud("v"+Integer.toString(i)));
-		}
-	}
-	
-	public void addEdge(int source, int destination, int weight){
-		Noeud s = Noeuds.get(source);
-		Edge new_edge = new Edge(Noeuds.get(destination),weight);
-		s.neighbours.add(new_edge);
-	}
-	
-	public ArrayList<Noeud> getNoeuds() {
-		return Noeuds;
-	}
-	
-	public Noeud getNoeud(int vert){
-		return Noeuds.get(vert);
-	}
+    private List<Noeud> noeuds;
+
+    public Graphe(int nombreDeNoeud) {
+        noeuds = new ArrayList<>(nombreDeNoeud);
+        for (int i = 0; i < nombreDeNoeud; i++) {
+            noeuds.add(new Noeud("v" + i));
+        }
+    }
+
+    public void addEdge(int source, int destination, double valeur) {
+        Noeud s = noeuds.get(source);
+        Edge e = new Edge(noeuds.get(destination), valeur);
+        s.filsList.add(e);
+    }
+
+    public Noeud getNoeud(int indice) {
+        return noeuds.get(indice);
+    }
+
+    public List<Noeud> getNoeuds() {
+        return noeuds;
+    }
 }
