7b193101536967db209bab675fd37e6ae718da8e
mohmdamine <156541315+mohmdamine@users.noreply.github.com>
2025-04-06 23:11:48 +0200

    Update Algorithm.java

--- dijkstra1_pfe/src/dijkstra1_pfe/dijkstra/Algorithm.java
+++ dijkstra1_pfe/src/dijkstra1_pfe/dijkstra/Algorithm.java
@@ -2,33 +2,29 @@ package dijkstra1_pfe.dijkstra;
 
 import java.util.*;
 
+
+
 public class Algorithm {
-	
-	public void calculer(Noeud source) {
-		source.minDistance = 0;
-		PriorityQueue<Noeud> queue = new PriorityQueue<>();
-		queue.offer(source);
-	
-		while(!queue.isEmpty()){
-		
-			Noeud u = queue.poll(); 
-	
-			for(Edge neighbour:u.neighbours){
-				Double newDist = u.minDistance+neighbour.weight;
-			
-				if(neighbour.suivant.minDistance>newDist){ 
-				
-					queue.remove(neighbour.suivant);
-					neighbour.suivant.minDistance = newDist;
-				
-				
-					neighbour.suivant.path = new LinkedList<Noeud>(u.path);
-					neighbour.suivant.path.add(u);
-				
-				
-					queue.add(neighbour.suivant);					 
-				}
-			}
-		}
-	}
+    public void calculer(Noeud source) {
+        source.minDistance = 0;
+        PriorityQueue<Noeud> queue = new PriorityQueue<>();
+        queue.add(source);
+
+        while (!queue.isEmpty()) {
+            Noeud u = queue.poll();
+
+            for (Edge e : u.filsList) {
+                Noeud v = e.suivant;
+                double distanceParU = u.minDistance + e.valeur;
+                if (distanceParU < v.minDistance) {
+                    queue.remove(v);
+                    v.minDistance = distanceParU;
+                    v.pere = u;
+                    v.trajet = new LinkedList<>(u.trajet);
+                    v.trajet.add(u);
+                    queue.add(v);
+                }
+            }
+        }
+    }
 }
